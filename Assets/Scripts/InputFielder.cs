729e3905c3a4f5399a98f0649f65ba35c0a98b39
Fashons <artemav2006@mail.ru>
2022-11-22 17:06:16 +1000

    добавлены команды

--- Assets/Scripts/InputFielder.cs
+++ Assets/Scripts/InputFielder.cs
@@ -4,11 +4,38 @@ using UnityEngine.UI;
 
 public class InputFielder : MonoBehaviour
 {
+    public int STEPSIZE = 2;
     public InputField field; // вставь сюда через инспектор
-    public ScriptableObject Movement;
+    //public GameObject Player;
 
     public void SomeMethod()
     {
-        Debug.Log(field.text);
+        
+        string[] fullCommand = field.text.Split('(');
+        string command = fullCommand[0].Split('.')[1];
+        int value = 0;
+        int.TryParse(fullCommand[1].TrimEnd(')'), out value);
+
+        switch (command)
+        {
+            case "move":
+                Debug.Log($"Команда перемещения на {value} шагов выполнена");
+                transform.position = new Vector3(transform.position.x + STEPSIZE, transform.position.y, transform.position.z);
+                break;
+
+            case "left":
+                Debug.Log("Команда поворота влево выполнена");
+                transform.Rotate(0.0f, -90.0f, 0.0f);
+                break;
+
+            case "right":
+                Debug.Log("Команда поворота вправо выполнена");
+                transform.Rotate(0.0f, 90.0f, 0.0f);
+                break;
+
+            case "jump":
+                Debug.Log($"Команда прыжка {value} раз выполнена");
+                break;
+        }
     }
 }
