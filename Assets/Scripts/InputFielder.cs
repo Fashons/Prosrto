18548c58edde9e06289dc5d28a4aacf54f88b205
Kvanto79 <g.kazakov@kvantorium79.ru>
2022-12-08 18:59:55 +1000

    Реализовано плавное движение персонажа

--- Assets/Scripts/InputFielder.cs
+++ Assets/Scripts/InputFielder.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 public class InputFielder : MonoBehaviour
 {
     public int STEPSIZE = 2;
-    public InputField field; // вставь сюда через инспектор
+    public InputField field;
+
+    private double currentValue = 0.0;
+    private double limit = 0.0;
 
     public void SomeMethod()
     {
-        
         string[] fullCommand = field.text.Split('(');
         string command = fullCommand[0].Split('.')[1];
         int value = 0;
@@ -18,22 +20,30 @@ public class InputFielder : MonoBehaviour
         switch (command)
         {
             case "move":
-                Debug.Log($" оманда перемещени€ на {value} шагов выполнена");
-                for (int i = 0; i < value; i++)
-                {
-                    transform.position += (transform.forward * STEPSIZE);
-                }
+                limit = value * STEPSIZE;
+                currentValue = 0.0;
                 break;
 
             case "left":
-                Debug.Log(" оманда поворота влево выполнена");
                 transform.Rotate(Vector3.up, -90f);
                 break;
 
             case "right":
-                Debug.Log(" оманда поворота вправо выполнена");
                 transform.Rotate(Vector3.up, 90f);
                 break;
         }
+    }
+
+
+    void Update()
+    {
+        if (currentValue < limit)
+        {
+            var distance = Time.deltaTime * STEPSIZE;
+            transform.Translate(Vector3.forward * distance);
+            currentValue += distance;
+        }
+        // else
+        //     currentValue = limit = 0;
     }
 }
