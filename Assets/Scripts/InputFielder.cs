1451badecfec1b31e9d9df498240e896b71f3b20
Fashons <artemav2006@mail.ru>
2022-12-13 17:02:06 +1000

    +_+

--- Assets/Scripts/InputFielder.cs
+++ Assets/Scripts/InputFielder.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,14 +10,87 @@ public class InputFielder : MonoBehaviour
 
     private double currentValue = 0.0;
     private double limit = 0.0;
+    private float delay = 0;
+
+    private Queue<CommandBlock> queue = new Queue<CommandBlock>();
+
 
     public void SomeMethod()
     {
-        string[] fullCommand = field.text.Split('(');
-        string command = fullCommand[0].Split('.')[1];
-        int value = 0;
-        int.TryParse(fullCommand[1].TrimEnd(')'), out value);
+        string[] lines = field.text.Split('\n');
 
+        foreach (var line in lines)
+        {
+            string[] fullCommand = line.Split('(');
+            string command = fullCommand[0].Split('.')[1];
+            int value = 0;
+            int.TryParse(fullCommand[1].TrimEnd(')'), out value);
+
+            queue.Enqueue(new CommandBlock() { command = command, value = value });
+        } 
+    }
+
+    void Update()
+    {
+        if (currentValue < limit)
+        {
+            var distance = Time.deltaTime * STEPSIZE;
+            transform.Translate(Vector3.forward * distance);
+            currentValue += distance;
+        }
+        else
+        {
+            while (delay < 2.0f)
+            {
+                delay += Time.deltaTime;
+            }
+
+            if (queue.Count > 0)
+            {
+                var queuedElement = queue.Dequeue();
+                RunCommand(queuedElement.command, queuedElement.value);
+            }
+        }
+        if (currentValue < limit)
+        {
+            var rotateR = Time.deltaTime * 90f;
+            transform.Rotate(Vector3.up, rotateR);
+            currentValue += rotateR;
+        }
+        else
+        {
+            if (queue.Count > 0)
+            {
+                var queuedElement = queue.Dequeue();
+                RunCommand(queuedElement.command, queuedElement.value);
+            }
+        }
+        if (currentValue < limit)
+        {
+            var rotateL = Time.deltaTime * -90f;
+            transform.Rotate(Vector3.up, rotateL);
+            currentValue += rotateL;
+        }
+        else
+        {
+            if (queue.Count > 0)
+            {
+                var queuedElement = queue.Dequeue();
+                RunCommand(queuedElement.command, queuedElement.value);
+            }
+        }
+    }
+
+
+    internal class CommandBlock
+    {
+        public string command;
+        public int value;
+    }
+    
+
+    private void RunCommand(string command, int value)
+    {
         switch (command)
         {
             case "move":
@@ -32,18 +106,5 @@ public class InputFielder : MonoBehaviour
                 transform.Rotate(Vector3.up, 90f);
                 break;
         }
-    }
-
-
-    void Update()
-    {
-        if (currentValue < limit)
-        {
-            var distance = Time.deltaTime * STEPSIZE;
-            transform.Translate(Vector3.forward * distance);
-            currentValue += distance;
-        }
-        // else
-        //     currentValue = limit = 0;
     }
 }
