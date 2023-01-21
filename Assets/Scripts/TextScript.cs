277f7187d261a31b09b8be65bd089a784f7f2461
Fash <artemav2006@mail.ru>
2023-01-21 14:41:46 +1000

    Обновление текста, СтенаGameOver.

--- Assets/Scripts/TextScript.cs
+++ Assets/Scripts/TextScript.cs
@@ -7,20 +7,27 @@ public class TextScript : MonoBehaviour
 {
     public Text TextGameObject;
     private string text;
+    private Text Text;
 
     private void Start()
     {
         text = TextGameObject.text;
         TextGameObject.text = "";
         StartCoroutine(TextCoroutine());
-    }
+        Invoke("TextSvitcher", 3f);
 
+    }
+    void TextSvitcher()
+    {
+        Text = GameObject.Find("TextOnDisplay").GetComponent<Text>();
+        Text.text = "Давай научу тебя играть!";
+    }
     IEnumerator TextCoroutine()
     {
         foreach(char abc in text)
         {
             TextGameObject.text += abc;
-            yield return new WaitForSeconds(0.09f);
+            yield return new WaitForSeconds(0.10f);
         }
     }
 }
---
+++ Assets/Scripts/WallGameOver.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class WallGameOver : MonoBehaviour
+{
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene("NewScene");
+        }
+    }
+}
